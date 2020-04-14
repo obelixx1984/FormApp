f9f98e79ddc128b447983c91d0f15943835e172d
Kamil <karbowiakk@wp.pl>
2020-04-14 12:45:02 +0200

    Panel Użytkownika

--- FormApp-SPA/src/app/app.module.ts
+++ FormApp-SPA/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { LogowanieComponent } from './logowanie/logowanie.component';
@@ -11,12 +14,21 @@ import { AuthService } from './_services/auth.service';
 import { PanelComponent } from './panel/panel.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { DefaultComponent } from './shared/components/default/default.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       LogowanieComponent,
-      PanelComponent
+      PanelComponent,
+      HeaderComponent,
+      FooterComponent,
+      SidebarComponent,
+      DefaultComponent
    ],
    imports: [
       BrowserModule,
@@ -24,7 +36,11 @@ import { appRoutes } from './routes';
       TabsModule.forRoot(),
       FormsModule,
       RouterModule.forRoot(appRoutes),
-      ShowHidePasswordModule
+      ShowHidePasswordModule,
+      BrowserAnimationsModule,
+      MatSidenavModule,
+      MatDividerModule,
+      MatToolbarModule
    ],
    providers: [
       AuthService,
